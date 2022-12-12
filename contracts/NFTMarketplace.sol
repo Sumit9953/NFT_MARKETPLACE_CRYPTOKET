ec89773672581ffafb10d14955c1a838bc786d82
sumit <sumitsantosh555@gmail.com>
2022-12-12 17:17:08 +0530

    create market sale

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -92,4 +92,47 @@ contract NFTMarketplace is ERC721URIStorage {
 
         _transfer(msg.sender, address(this), tokenId);
     }
+
+    function createMarketSale(uint256 tokenId) public payable {
+        uint price = idToMarketItem[tokenId].price;
+
+        require(msg.value == price, "Please submit the asking price in order to complete the purchase");
+
+        idToMarketItem[tokenId].owner = payable(msg.sender);
+        idToMarketItem[tokenId].sold = true;
+        idToMarketItem[tokenId].seller = payable(address(0));
+
+        _itemsSold.increment();
+
+        _transfer(address(this), msg.sender, tokenId);
+
+        payable(owner).transfer(listingPrice);
+        payable(idToMarketItem[tokenId].seller).transfer(msg.value);
+    }
+
+    function fetchMarketItem() public view returns (MarketItem[] memory){
+        uint itemCount = _tokenIds.current();
+        uint unsoldItemCount = _tokenIds.current() - _itemsSold.current();
+        uint currentIndex = 0;
+
+        MarketItem[] memory items = new MarketItem[](unsoldItemCount);
+
+        for(uint i = 0;i<itemCount;i++){
+            if(idToMarketItem[i+1].owner == address(this)){
+                uint currentId = i + 1;
+
+                MarketItem storage currentItem = idToMarketItem[currentId];
+
+                items[currentIndex] = currentItem;
+
+                currentIndex += 1;
+            }
+        }
+        return items;
+    }
+
+        function fetchMyNFTs() public view returns (MarketItem[] memory){
+            
+        }
+
 }
