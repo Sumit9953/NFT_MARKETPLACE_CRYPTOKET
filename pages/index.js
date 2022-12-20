3d5d07b76d6ce8ee33a3e36e258944599ae3f27a
sumit <sumitsantosh555@gmail.com>
2022-12-20 15:24:33 +0530

    fix so,e small bug

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { useTheme } from 'next-themes';
 import Image from 'next/image';
 
 import { NFTContext } from '../context/NFTContext';
-import { Banner, CreatorCard, NFTCard, SearchBar } from '../components';
+import { Banner, CreatorCard, Loader, NFTCard, SearchBar } from '../components';
 import images from '../assets';
 import { makeId } from '../utils/makeId';
 import { getCreators } from '../utils/getTopCreators';
@@ -16,6 +16,7 @@ const Home = () => {
   const [nftsCopy, setNftsCopy] = useState([]);
   const { theme } = useTheme();
   const [activeSelect, setActiveSelect] = useState('Recently added');
+  const [isLoading, setIsLoading] = useState(true);
 
   const parentRef = useRef(null);
   const scrollRef = useRef(null);
@@ -25,6 +26,7 @@ const Home = () => {
       .then((items) => {
         setNfts(items);
         setNftsCopy(items);
+        setIsLoading(false);
       });
   }, []);
 
@@ -101,28 +103,35 @@ const Home = () => {
     <div className="flex justify-center sm:px-4 p-12 ">
       <div className="w-full minmd:w-4/5">
         <Banner
-          name="Discover, collect, and sell extraordinary NFTs"
+          name={(<>Discover, collect, and sell <br /> extraordinary NFTs </>)}
           childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left"
           parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
         />
-        <div>
-          <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl
-        minlg:text-4xl font-semibold ml-4 xs:ml-0 "
-          >
-            Best Creaters
+
+        {!isLoading && !nfts.length ? (
+          <h1 className="font-popiins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 xs:ml-0">
+            That&apos;s weird... NO NFTs for sale!
           </h1>
-          <div className="relative flex-1 max-w-full flex mt-3" ref={parentRef}>
-            <div className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none" ref={scrollRef}>
-              {topCreators.map((creator, i) => (
-                <CreatorCard
-                  key={creator.seller}
-                  rank={i + 1}
-                  creatorImage={images[`creator${i + 1}`]}
-                  creatorName={shortenAddress(creator.seller)}
-                  creatorEths={creator.sum}
-                />
-              ))}
-              {/* {[6, 7, 8, 9, 10].map((i) => (
+        ) : isLoading ? <Loader /> : (
+          <>
+            <div>
+              <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl
+        minlg:text-4xl font-semibold ml-4 xs:ml-0 "
+              >
+                Top Sellers
+              </h1>
+              <div className="relative flex-1 max-w-full flex mt-3" ref={parentRef}>
+                <div className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none" ref={scrollRef}>
+                  {topCreators.map((creator, i) => (
+                    <CreatorCard
+                      key={creator.seller}
+                      rank={i + 1}
+                      creatorImage={images[`creator${i + 1}`]}
+                      creatorName={shortenAddress(creator.seller)}
+                      creatorEths={creator.sum}
+                    />
+                  ))}
+                  {/* {[6, 7, 8, 9, 10].map((i) => (
                 <CreatorCard
                   key={`creator-${i}`}
                   rank={i}
@@ -131,50 +140,50 @@ const Home = () => {
                   creatorEths={10 - i * 0.5}
                 />
               ))} */}
-              {!hideButtons && (
-              <>
-                <div onClick={() => handleScroll('left')} className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer left-0">
-                  <Image
-                    src={images.left}
-                    layout="fill"
-                    objectFit="conatin"
-                    alt="left_arrow"
-                    className={theme === 'light' && 'filter invert'}
-                  />
+                  {!hideButtons && (
+                  <>
+                    <div onClick={() => handleScroll('left')} className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer left-0">
+                      <Image
+                        src={images.left}
+                        layout="fill"
+                        objectFit="conatin"
+                        alt="left_arrow"
+                        className={theme === 'light' && 'filter invert'}
+                      />
+                    </div>
+                    <div onClick={() => handleScroll('right')} className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer right-0">
+                      <Image
+                        src={images.right}
+                        layout="fill"
+                        objectFit="conatin"
+                        alt="left_arrow"
+                        className={theme === 'light' && 'filter invert'}
+                      />
+                    </div>
+                  </>
+                  )}
                 </div>
-                <div onClick={() => handleScroll('right')} className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer right-0">
-                  <Image
-                    src={images.right}
-                    layout="fill"
-                    objectFit="conatin"
-                    alt="left_arrow"
-                    className={theme === 'light' && 'filter invert'}
-                  />
-                </div>
-              </>
-              )}
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="mt-10">
-          <div className="flexBetween mx-4 xs:mx-0 minlg:mx-8 sm:flex-col sm:items-start">
-            <h1 className=" flex-1 before:first:font-poppins dark:text-white text-nft-black-1 text-2xl
+            <div className="mt-10">
+              <div className="flexBetween mx-4 xs:mx-0 minlg:mx-8 sm:flex-col sm:items-start">
+                <h1 className=" flex-1 before:first:font-poppins dark:text-white text-nft-black-1 text-2xl
                  minlg:text-4xl font-semibold sm:mb-4"
-            >
-              Hot Bids
-            </h1>
-            <div className="flex-2 sm:w-full flex flex-row sm:flex-col">
-              <SearchBar
-                activeSelect={activeSelect}
-                setActiveSelect={setActiveSelect}
-                handleSearch={onHandleSearch}
-                clearSearch={onClearSearch}
-              />
-            </div>
-          </div>
-          <div className="mt-3 w-full flex flex-wrap justify-start md:justify-center">
-            {nfts.map((nft) => <NFTCard key={nft.tokenId} nft={nft} />)}
-            {/* {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
+                >
+                  HOT NFTs
+                </h1>
+                <div className="flex-2 sm:w-full flex flex-row sm:flex-col">
+                  <SearchBar
+                    activeSelect={activeSelect}
+                    setActiveSelect={setActiveSelect}
+                    handleSearch={onHandleSearch}
+                    clearSearch={onClearSearch}
+                  />
+                </div>
+              </div>
+              <div className="mt-3 w-full flex flex-wrap justify-start md:justify-center">
+                {nfts.map((nft) => <NFTCard key={nft.tokenId} nft={nft} />)}
+                {/* {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
               <NFTCard
                 key={`nft-${i}`}
                 nft={{
@@ -187,8 +196,10 @@ const Home = () => {
                 }}
               />
             ))} */}
-          </div>
-        </div>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
